84ac5d1b1b325fe9249860aa7c5b9f696548b936
Deepesh Hada <deepesh.hada@gmail.com>
2020-02-20 15:15:06 +0530

    Modified CNN architecture: more filters per layer and max pooling instead of average pooling

--- classes/CNN.py
+++ classes/CNN.py
@@ -5,20 +5,22 @@ class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
         self.cnn_model = nn.Sequential(
-            nn.Conv2d(1, 6, 5),
-            nn.ReLU(inplace=True),
+            nn.Conv2d(1, 12, 5, padding=2),
+            nn.ReLU(),
             nn.MaxPool2d(2, stride=2),
-            nn.Conv2d(6, 16, 5),
-            nn.ReLU(inplace=True),
+            nn.Conv2d(12, 20, 5),
+            nn.ReLU(),
             nn.MaxPool2d(2, stride=2)
         )
         self.fc_model = nn.Sequential(
-            nn.Linear(256, 120),
-            nn.ReLU(inplace=True),
-            nn.Linear(120, 84),
-            nn.ReLU(inplace=True),
-            nn.Linear(84, 10),
-            nn.Softmax(dim=1)
+            nn.Linear(500, 250),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Dropout(p=0.2),
+            nn.Linear(250, 200),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Linear(200, 84),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Linear(84, 10)
         )
 
     def forward(self, x):
